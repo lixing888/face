633ca4142b7867f227352f0b16fbddfd29cf0342
lixing <85210279@qq.com>
2020-05-28 23:07:16 +0800

    代码优化

--- src/main/java/com/baidu/FaceApplication.java
+++ src/main/java/com/baidu/FaceApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * @author lixing
+ * http://127.0.0.1:8088/face/swagger-ui.html
+ */
 @EnableSwagger2
 @SpringBootApplication
 public class FaceApplication {
